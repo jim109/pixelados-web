21494ffe8b7649de3fba6aa6d148f47230588294
Jimmy Osma <josma109@gmail.com>
2023-10-23 23:01:29 -0500

    [UPDATE] page contact

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,19 @@ module.exports = {
 		fontFamily: {
 			sans: ['Gotham SSm A', 'Gotham SSm B', 'sans-serif']
 		},
+		colors: {
+			'primary-100': '#2C3E50',
+			'primary-200':'#57687c',
+			'primary-300':'#b4c7dd',
+			'accent-100':'#F7CAC9',
+			'accent-200':'#926b6a',
+			'text-100':'#333333',
+			'text-200':'#5c5c5c',
+			'text-300':'#F2F2F2',
+			'bg-100':'#F2F2F2',
+			'bg-200':'#e8e8e8',
+			'bg-300':'#bfbfbf',
+		},
 		extend: {},
 	},
 	plugins: [],
